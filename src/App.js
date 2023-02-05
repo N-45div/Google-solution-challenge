034f9dd0538899017b8af26459df1ff788431de1
N DIVIJ <123795325+N-45div@users.noreply.github.com>
2023-02-06 00:53:54 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import { Routes, Route } from "react-router-dom"
 import './App.css';
+import addNotification from 'react-push-notification';
 import Home from './Home';
 import Navbar from './components/Nav/Navbar'
 import Footer from "./Footer";
 function App() {
+  const clickTONotify = ()=>{
+    addNotification({
+      title : "Disaster alert",
+      message : "Click on evacuation routes",
+      duration : 4000,
+      native : true,
+      onClick: ()=> console.log('Push Notification'),
+    });
+  }
   return (
     <div className="App">
       <Navbar/>
